ae9c299a9f24227604fcf1018798b54502a4c075
Mehfoozulhaq <mehfooz968268@gmail.com>
2024-11-13 20:00:38 +0530

    Create prime_no.cpp

---
+++ prime_no.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a number\n";
+    cin>>a;
+    if (a%2==0)
+    {
+        cout<<"number is even\n";
+    }
+    else 
+    cout<<"number is odd\n";
+
+    return 0;
+    
+}
